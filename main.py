d1c2a53e0fae8fe854bb8d182c49ebc5a4fae144
Prince <riches.prince10@gmail.com>
2022-02-25 16:20:01 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+ # Fibonacci Series For Recursion
+
+def recur_fibo(n):
+   if (n <= 1):
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 15
+
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))               
+
+
+
+
+# Fibonacci series for the looping
+
+nterms = int(input("In how many terms? "))
+
+
+n1, n2 = 0, 1
+count = 0
+
+
+if nterms <= 0:
+   print("Please enter a positive integer")
+
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       
+       n1 = n2
+       n2 = nth
+       count += 1
